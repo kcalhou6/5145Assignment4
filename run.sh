a078232e2182267c9054326e57826ce0e50fb76f
KC <kcalhou6@uncc.edu>
2016-10-16 18:54:06 -0400

    re-order full run.sh

--- run.sh
+++ run.sh
@@ -18,6 +18,16 @@ for t in 1 2 4 8 16; do ./reduce 100000000 $t 1 1000; done
 # dynamic,100000 scheduling with  1, 2, 4, 8, 16 threads
 for t in 1 2 4 8 16; do ./reduce 100000000 $t 1 100000; done
 
+g++ FindFirst_A4.cpp -o first -fopenmp -O3 -march=native -std=gnu++0x
+#Time complexity n
+for t in 1 2 4 8 16; do ./first 100000000 0 $t 0; done
+#Time complexity pos with 1, 2, 4, 8, 16 threads
+for t in 1 2 4 8 16; do ./first 100000000 70234 $t 1; done
+
+g++ PrefixSum_A4.cpp -o prefix -fopenmp -O3 -march=native -std=gnu++0x
+# prefix sum of array length 10^9 for 1 2 4 8 16 threads
+for to in 1 2 4 8 16; do ./prefix 1000000000 $t; done
+
 g++ NumericalIntegration_A4.cpp -o integrate -fopenmp -O3 -march=native -std=gnu++0x
 # dynamic,1 scheduling with 1, 2, 4, 8, 16 threads (x^2 from 1 to 10 over 10^3 points and 10 intensity)
 for t in 1 2 4 8 16; do ./integrate 1 10 1000 0 10 $t 1; done
@@ -35,13 +45,3 @@ for t in 1 2 4 8 16; do ./integrate 1 10 1000000000 0 1000 $t 1; done
 for t in 1 2 4 8 16; do ./integrate 1 10 1000000000 0 10 $t 1000; done
 # dynamic,1000 scheduling with  1, 2, 4, 8, 16 threads (x^2 from 1 to 10 over 10^9 points and 1000 intensity)
 for t in 1 2 4 8 16; do ./integrate 1 10 1000000000 0 1000 $t 1000; done
-
-g++ FindFirst_A4.cpp -o first -fopenmp -O3 -march=native -std=gnu++0x
-#Time complexity n
-for t in 1 2 4 8 16; do ./first 100000000 0 $t 0; done
-#Time complexity pos with 1, 2, 4, 8, 16 threads
-for t in 1 2 4 8 16; do ./first 100000000 70234 $t 1; done
-
-g++ PrefixSum_A4.cpp -o prefix -fopenmp -O3 -march=native -std=gnu++0x
-# prefix sum of array length 10^9 for 1 2 4 8 16 threads
-for to in 1 2 4 8 16; do ./prefix 1000000000 $t; done
